02b217c14ea9876fe93a2974bf3cf046f897af67
erik-ringen <erik-ringen@users.noreply.github.com>
2021-02-18 21:39:04 -0500

    processing script to create data csv and phylogeny

---
+++ processing.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(phytools)
+
+######################################
+d <- read.csv("data-raw/SCCS-var1-2000.csv", stringsAsFactors = F) # reading in SCCS dataset, should just give reduced data for publication
+d <- d[-187,] # dropping extraneous row
+
+# Selecting study variables from SCCS codebook
+d <- d[,c("socname", "v149", "v150", "v151", "v152", "v153", "v154", "v155", "v156", "v157", "v158", "v204", "v20", "v820", "v179", "v181", "v200", "v201")]
+
+sccs_tree <- read.nexus("data-raw/sccs_supertree.nex")
+setdiff(sccs_tree$tip.label, d$socname) # checking for discrepancies between phylo tree names and dataframe names
+
+# "MISSING" indicates that genetic data missing for the target pop, position based on linguistic data
+d[c(1,2,14,25,27,29,30,33,39,46,48,53,59,63,66,82,93,97,102,106,124,133,134,135,136,137,138,139,140,143,144,146,158,164,167,174,176,178), "socname"] <-  c("Nama_Hottentot", "Kung_Bushmen", "Nkundo_Mongo", "Pastoral_Fulani", "Massa_Masa", "Fur_Darfur", "Otoro_Nuba", "MISSING_Kafa_Kaffa", "Kenuzi_Nubians","Rwala_Bedouin", "Gheg_Albanians", "Yurak_Samoyed", "Punjabi_West", "Uttar_Pradesh", "Khalka_Mongols", "Negri_Sembilan", "MISSING_Kimam", "New_Ireland", "Mbau_Fijians", "Western_Samoans", "Copper_Eskimo", "MISSING_Twana", "MISSING_Yurok", "MISSING_Pomo_Eastern", "Yokuts_Lake", "Paiute_North", "MISSING_Klamath","MISSING_Kutenai", "Gros_Ventre", "MISSING_Omaha", "MISSING_Huron", "MISSING_Natchez", "Cuna_Tule", "Carib_Barama", "Cubeo_Tucano", "MISSING_Nambicuara", "MISSING_Timbira", "MISSING_Botocudo")
+
+setdiff(sccs_tree$tip.label, d$socname) # should be 0
+
+sccs_tree <- drop.tip(sccs_tree, subset(sccs_tree$tip.label, !(sccs_tree$tip.label %in% d$socname)))
+
+# Putting the phylogeny and the data frame in the same order, this is very important!
+d <- d[match(sccs_tree$tip.label, d$socname),]
+
+# Now we can remove the "MISSING" from the labels
+d$socname <- ifelse(substr(d$socname, 1, nchar("MISSING")) == "MISSING", substr(d$socname, nchar("MISSING")+2, nchar(d$socname)), d$socname)
+
+# Do the same for phylogeny
+sccs_tree$tip.label <- ifelse(substr(sccs_tree$tip.label, 1, nchar("MISSING")) == "MISSING", substr(sccs_tree$tip.label, nchar("MISSING")+2, nchar(sccs_tree$tip.label)), sccs_tree$tip.label)
+
+# Export processed files for analysis
+write.csv(d, "data_analysis.csv", row.names = F)
+write.tree(sccs_tree, "SCCS_supertree.tre")
